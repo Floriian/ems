61959f0d74b3f4990e08e309cc82e340d2536bb4
Floriian <lovasifloriian@outlook.hu>
2024-02-08 21:56:35 +0100

    feat: install mui for admin.

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -4,6 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
