00e35ed7e5c341620c6eb03026659c095a6a3145
Floriian <lovasifloriian@outlook.hu>
2024-02-24 15:35:10 +0100

    feat: auth

--- apps/admin/src/app/components/layout/Layout.tsx
+++ apps/admin/src/app/components/layout/Layout.tsx
@@ -1,10 +1,8 @@
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -12,16 +10,23 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../features/auth';
+import { useEffect, useState } from 'react';
 
 const drawerWidth = 240;
 
 export function Layout() {
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-  const [isClosing, setIsClosing] = React.useState(false);
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
+
+  const { isLoggedIn } = useAuth()
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoggedIn) navigate("/")
+  }, [isLoggedIn, navigate]);
 
   const handleDrawerClose = () => {
     setIsClosing(true);
